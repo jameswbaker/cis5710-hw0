d8bba5ce441037901369962b2158b4f0209a9308
kirby1926 <bakj@seas.upenn.edu>
2024-01-25 22:30:31 +0000

    first commit

---
+++ hw0/uppercase.disasm.s
@@ -0,0 +1,12 @@
+
+uppercase.bin:     file format elf32-littleriscv
+
+
+Disassembly of section .text:
+
+00010074 <_start>:
+   10074:	ffff2517          	auipc	a0,0xffff2
+   10078:	f8c50513          	addi	a0,a0,-116 # 2000 <__DATA_BEGIN__>
+
+0001007c <end_program>:
+   1007c:	0000006f          	j	1007c <end_program>
